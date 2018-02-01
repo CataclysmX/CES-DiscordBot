3e9a26b460823058ad20bf6a4d59c7d1b9e24b80
mamathieu <mamathieu@users.noreply.github.com>
2018-02-01 23:58:02 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on('message', msg => {
   if (msg.content.startsWith(prefix + "test")) {
   msg.delete();
   Logs();
-  msg.channel.send(member.user.id);
+  msg.channel.send(message.author.id);
   }
 
 
